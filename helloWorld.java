87e9931d32e95f0b505994405866dea9e3b2bd81
lucarbone <lcarbone@cerebotani.it>
2023-10-28 08:12:10 +0200

    il programma fa la somma

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,9 @@ import java.io.*;
 
 public class helloWorld{
 	public static void main(String args[]){
+		int n = 5;
+		int n2 = 10;
 		System.out.println("Hello World!!");
+		System.out.println("Sommina :"+(n+n2));
 	}
 }
